38196fb25e540584f03b4d8b3d8d39e96adc263d
riyasklb <riyasklb89@gmail.com>
2024-08-02 01:11:21 +0530

    localdummy api

---
+++ server2.js
@@ -0,0 +1,36 @@
+import { createServer } from 'http';
+const PORT = process.env.PORT;
+const user = [{
+    id: 1,
+    name: 'Sample Data1',
+    description: 'This is a sample API data.'
+}, {
+    id: 2,
+    name: 'Sample Data2',
+    description: 'This is a sample API data.'
+}, {
+    id: 3,
+    name: 'Sample Data3',
+    description: 'This is a sample API data.'
+},
+];
+
+const server = createServer((req, res) => {
+    if (req.url === '/api/users' && req.method === "GET") {
+        res.setHeader('Content-Type', 'application/json');
+        res.write(JSON.stringify(user));
+        res.end();
+    } else {
+        res.statusCode = 404
+        res.setHeader('Content-Type', 'application/json');
+        res.write(JSON.stringify({ message: 'not fount' }));
+        res.end();
+    }
+
+});
+
+
+server.listen(PORT, () => {
+    console.log(`server on port ${PORT}`);
+
+});
